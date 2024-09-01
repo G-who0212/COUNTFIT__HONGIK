05580f4a65c57678356071bcfe05c23a2430be1b
G-who0212 <gwho990212@gmai.com>
2024-09-01 15:41:21 +0900

    for test2

--- backend/countfitBackend/account/urls.py
+++ backend/countfitBackend/account/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     path('signup/', UserRegisterAPIView.as_view()),
     path('signin/', UserLoginAPIView.as_view()),
-    path('refresh/', TokenRefreshView.as_view()), # FE에서 만료된 access Token을 보냈을 때
-    path('userinfo/', UserInfoListAPIView.as_view()) # account/userinfo/?nickname=hoo
+    # path('refresh/', TokenRefreshView.as_view()), # FE에서 만료된 access Token을 보냈을 때
+    # path('userinfo/', UserInfoListAPIView.as_view()) # account/userinfo/?nickname=hoo
 ]
