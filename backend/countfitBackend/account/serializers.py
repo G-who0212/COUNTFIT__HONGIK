7be0c3c2f861f7343ee70e9d5d74e26dfbe07804
G-who0212 <gwho990212@gmai.com>
2024-09-01 18:06:29 +0900

    password2 available

--- backend/countfitBackend/account/serializers.py
+++ backend/countfitBackend/account/serializers.py
@@ -5,14 +5,31 @@ from .models import User, Record
 User = get_user_model()
 
 
+from rest_framework import serializers
+from .models import User
+
 class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)  # password2 필드 추가
+
     class Meta:
         model = User
-        fields = "__all__"
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ['email', 'password', 'password2', 'nickname', 'gender', 'age']  # 필요한 필드만 나열
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
+    # password와 password2가 일치하는지 검증
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    # create 메서드 수정: password2는 저장하지 않음
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        validated_data.pop('password2')  # password2 필드를 제거
+        user = User.objects.create_user(**validated_data)  # create_user 메서드 사용
+        return user
+
 
 
 class UserLoginSerializer(serializers.ModelSerializer):
